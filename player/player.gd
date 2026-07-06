165683a71ac85f4ce0da8f7afa7002ea23fb5902
Jhon Esparragoza <jhonantonyesp@gmail.com>
2026-07-06 23:49:27 +0200

    añadido salto cortito y reticula centrada en pantalla

--- player/player.gd
+++ player/player.gd
@@ -28,5 +28,7 @@ func _physics_process(delta):
 	velocity.y -= 20.0 * delta
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = 10.0
+	elif Input.is_action_just_released("jump") and velocity.y > 0.0:
+		velocity.y = 5.0
 	
 	move_and_slide()
